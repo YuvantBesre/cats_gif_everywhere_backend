0f35ff7303137fbf51ca474af504c4f23086f209
Yuvant <yuvant.besre@medikabazaar.com>
2024-09-09 00:40:03 +0530

    Change database host

--- cats_gif_everywhere_server/settings.py
+++ cats_gif_everywhere_server/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'NAME': os.environ.get('DATABASE_NAME'),
         'USER': os.environ.get('DATABASE_USER'),
         'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-        'HOST': 'localhost',
+        'HOST': os.environ.get('DATABASE_HOST'),
         'PORT': '',
     }
 }
