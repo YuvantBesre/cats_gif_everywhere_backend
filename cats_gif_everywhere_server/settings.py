0f457828b8df0e27eb878e03ccbfdf3e8f506dd5
Yuvant <yuvant.besre@medikabazaar.com>
2024-09-09 00:24:39 +0530

    Add postgres

--- cats_gif_everywhere_server/settings.py
+++ cats_gif_everywhere_server/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'cats_gif_everywhere_server.wsgi.app'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
