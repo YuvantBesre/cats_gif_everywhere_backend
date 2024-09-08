0b2d4684761bbc3738b345d9ae7813a76d7553ef
Yuvant <yuvant.besre@medikabazaar.com>
2024-09-08 15:32:28 +0530

    Fix Edit issue

--- cats_everywhere/utils.py
+++ cats_everywhere/utils.py
@@ -1,4 +1,5 @@
 from django.utils.text import slugify
+import string, random
 
 def random_string_generator(size = 10, chars = string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
